d87c11c20b4fca33e7b3c3b656f6fd22deb7482b
Rafael Bertoni <programacao.curso2022@gmail.com>
2022-10-07 17:40:37 -0300

    início do programa de teste

---
+++ test/initial_test.py
@@ -0,0 +1,6 @@
+l = []
+
+for i in range(0, 11):
+    l.append(i)
+    print(l)
+
---
+++ test/tables.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 14:43:48 2022
+
+@author: berto
+"""
+
+import pandas as pd
+
+name = ["Marcos", "Sérgio", "Roberto"]
+age = [34, 55, 13]
+
